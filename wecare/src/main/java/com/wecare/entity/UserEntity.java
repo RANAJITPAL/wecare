bbe49b201eb4bde2f10dbae5c1615602abb9178b
Ranajit Pal <palranajit98@gmail.com>
2021-04-05 22:35:56 +0530

    add userEntity class

--- wecare/src/main/java/com/wecare/entity/UserEntity.java
+++ wecare/src/main/java/com/wecare/entity/UserEntity.java
@@ -1,11 +1,137 @@
 package com.wecare.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Table(name = "usertable")
 public class UserEntity {
 	
+	@Id
+	@GenericGenerator(name = "user_id", strategy = "com.wecare.utility.UserIdGenerator")
+    @GeneratedValue(generator = "user_id")  
+	private String userId;
+	
+	private String name;
+	private String password;
+	private String gender;
+	private String dateOfBirth;
+	private Long mobileNumber;
+	private String email;
+	private Long pincode;
+	private String city;
+	private String state;
+	private String country;
+	
+	public UserEntity() {
+		super();
+	}
+
+	public UserEntity(String name, String password, String gender, String dateOfBirth, Long mobileNumber,
+			String email, Long pincode, String city, String state, String country) {
+		super();
+		this.name = name;
+		this.password = password;
+		this.gender = gender;
+		this.dateOfBirth = dateOfBirth;
+		this.mobileNumber = mobileNumber;
+		this.email = email;
+		this.pincode = pincode;
+		this.city = city;
+		this.state = state;
+		this.country = country;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public Long getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(Long mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Long getPincode() {
+		return pincode;
+	}
+
+	public void setPincode(Long pincode) {
+		this.pincode = pincode;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
 	
 }
