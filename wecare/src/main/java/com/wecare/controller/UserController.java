f1b4cf84018d0c08d8b0625e093902c042ca0c8e
Ranajit Pal <palranajit98@gmail.com>
2021-04-05 22:49:06 +0530

    add userController methods

--- wecare/src/main/java/com/wecare/controller/UserController.java
+++ wecare/src/main/java/com/wecare/controller/UserController.java
@@ -1,8 +1,21 @@
 package com.wecare.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.wecare.entity.UserEntity;
+import com.wecare.repository.UserRepository;
 
 @Controller
 public class UserController {
 	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@GetMapping("/user/{userId}")
+	public UserEntity getUserProfile(@PathVariable(value = "userId") String userId) {
+		return userRepository.getOne(userId);
+	}
 }
