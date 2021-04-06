b37d9988493cbc990afd0a6d9c7bbb56a9bb145e
Ranajit Pal <palranajit98@gmail.com>
2021-04-06 10:58:51 +0530

    and createUser method

--- wecare/src/main/java/com/wecare/controller/UserController.java
+++ wecare/src/main/java/com/wecare/controller/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.wecare.entity.UserEntity;
 import com.wecare.repository.UserRepository;
@@ -17,5 +19,10 @@ public class UserController {
 	@GetMapping("/user/{userId}")
 	public UserEntity getUserProfile(@PathVariable(value = "userId") String userId) {
 		return userRepository.getOne(userId);
+	}
+	
+	@PostMapping("/user")
+	public UserEntity createUser(@RequestBody UserEntity userEntity) {
+		return userRepository.save(userEntity);
 	}
 }
